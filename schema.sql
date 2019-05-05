089fbd33eac9e1dc760417807339ba5268d99011
Stephen Chu <stephen.chu530@gmail.com>
2019-05-05 10:44:00 -0700

    refactor: redid comments and numeric col parameters

--- schema.sql
+++ schema.sql
@@ -1,20 +1,26 @@
 /*
-  Source code author: Michelle Ferreirae
-  URL: https://github.com/codefellows/seattle-301d56/blob/master/class-09/demo/data/schema.sql
+  The sql schema has to pretty much be on point and will look 'highly'
+  similar to many other schemas for this project. The base schema.sql
+  was taken from Course 301 instructor: Michelle Ferreirae.
+
+  The source of Michelle's schema.sql is:
+  https://github.com/codefellows/seattle-301d56/blob/master/class-09/demo/data/schema.sql
 */
 
+-- Drop all tables to start a clean slate
 DROP TABLE IF EXISTS weathers, yelps, movies, trails, events;
 DROP TABLE IF EXISTS locations;
 
-CREATE TABLE IF NOT EXISTS locations (
+-- Create the tables
+CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude NUMERIC(10, 7),
-    longitude NUMERIC(10, 7)
+    latitude NUMERIC,
+    longitude NUMERIC
   );
 
-CREATE TABLE IF NOT EXISTS weathers (
+CREATE TABLE weathers (
     id SERIAL PRIMARY KEY,
     forecast VARCHAR(255),
     time VARCHAR(255),
@@ -22,7 +28,7 @@ CREATE TABLE IF NOT EXISTS weathers (
     location_id INTEGER NOT NULL REFERENCES locations(id)
   );
 
-CREATE TABLE IF NOT EXISTS events (
+CREATE TABLE events (
   id SERIAL PRIMARY KEY,
   link VARCHAR(255),
   name VARCHAR(255),
@@ -32,24 +38,24 @@ CREATE TABLE IF NOT EXISTS events (
   location_id INTEGER NOT NULL REFERENCES locations(id)
 );
 
-CREATE TABLE IF NOT EXISTS movies (
+CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   overview VARCHAR(1000),
-  average_votes NUMERIC(10, 7),
+  average_votes NUMERIC,
   image_url VARCHAR(255),
-  popularity NUMERIC(10, 7),
+  popularity NUMERIC,
   released_on CHAR(10),
   created_at VARCHAR(255),
   location_id INTEGER NOT NULL REFERENCES locations(id)
 );
 
-CREATE TABLE IF NOT EXISTS yelps (
+CREATE TABLE yelps (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   image_url VARCHAR(255),
   price CHAR(5),
-  rating NUMERIC(10, 7),
+  rating NUMERIC,
   url VARCHAR(255),
   created_at VARCHAR(255),
   location_id INTEGER NOT NULL REFERENCES locations(id)
